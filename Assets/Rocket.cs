4f675cb2d228685bb8a55df08c48a1a386fc6ef1
lucasw89 <lucasw89@live.dk>
2019-06-15 14:31:56 +0200

    Got the rocket moving around
    Got the rocket moving using A, D and SPACE. Deleted some unsued files.

---
+++ Assets/Rocket.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    private Rigidbody rigidBody;
+
+    [SerializeField]
+    private float thrustSpeed = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = this.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessRemoteInput();
+
+
+    }
+
+    private void ProcessRemoteInput()
+    {
+        // Thrust if space is down
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidBody.AddRelativeForce(Vector3.up);
+        }
+        
+        // Can only rotate one way at a time.
+        if (Input.GetKey(KeyCode.A))
+        {
+            this.transform.Rotate(Time.deltaTime * thrustSpeed * Vector3.forward);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            this.transform.Rotate(Time.deltaTime * thrustSpeed * Vector3.back);
+        }
+        
+        
+
+
+    }
+}
